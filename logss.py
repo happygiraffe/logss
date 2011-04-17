2184cb0c037a8bff367dae256a1752d45c6cc8ed
Dominic Mitchell <dom@happygiraffe.net>
2011-04-17 23:20:32 +0100

    Initial version.

---
+++ logss.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""Log a row to a Google Spreadsheet."""
+
+__author__ = 'Dominic Mitchell <dom@happygiraffe.net>'
+
+
+import sys
+
+
+def main(argv):
+  print 'Hello world'
+
+
+if __name__ == '__main__':
+  main(sys.argv)
